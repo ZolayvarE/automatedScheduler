a0bc306ff272e57ec18f09b4d8a5cbd5b852232d
ZolayvarE <ZolayvarECU@gmail.com>
2017-01-03 11:49:45 -0800

    Implement passing in arguments from command line

--- index.js
+++ index.js
@@ -1,19 +1,19 @@
 var scrape = require('./scraper.js');
 var scripts = require('./scripts.js');
 
-var targetDate = scripts.date(0);
-
-if ((new Date).getDay() !== 0) {
-  scrape(targetDate, function (err, data) {
-    if (err) {
-      console.err(err);
-    } else {
-      console.log(data);
-      // scripts.sendEmail([]);
-    }
-  });
-} else {
-  console.err('Today is Sunday, you goof!');
-}
+var targetDate = scripts.date(Number(process.argv.slice(-1)[0]) || 0);
+  
+scrape(targetDate, function (err, data) {
+  if ((new Date).getDay() === 0) {
+    console.log('\nToday is Sunday, you goof!\n');
+  } else if (err) {
+    console.err(err);
+  } else if (process.argv.includes('broadcast')) {
+    console.log(data, '\n\n');
+    scripts.sendEmail(data);
+  } else {
+    console.log(data);
+  }
+});
 
 
